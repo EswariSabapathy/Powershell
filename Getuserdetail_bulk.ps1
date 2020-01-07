2e0fcf7202c709ea689faf0fd72512e9a8a380ea
EswariSabapathy <eswarionline@gmail.com>
2020-01-07 10:28:34 -0600

    Add files via upload

---
+++ Getuserdetail_bulk.ps1
@@ -0,0 +1,43 @@
+#param($p1)
+#$Script:args=""
+$FileName = "C:\IDM\Azure\Filename.csv"
+$username = "userid@domain.com"
+
+$pass = convertto-securestring (get-content secstring.txt) -key (1..16)
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $pass
+Connect-MsolService -Credential $cred
+Connect-AzureAD -Credential $cred
+$i=0
+
+Get-Content -Path $FileName |ForEach-Object  {
+   #$Script:args+= "`$$key=" + $PSBoundParameters["$key"] + "  "
+    $i++ 
+     $user = Get-AzureADUser -Filter "userPrincipalName eq '$_'"
+   $email = $user.mail
+    
+    #return $PSBoundParameters["$key"];
+   
+    $userdetail = $(Get-MsolUser -UserPrincipalName $_).StrongAuthenticationUserDetails 
+   
+   $valuesUser = $userdetail | Select-Object -Property Email,AlternativePhoneNumber,PhoneNumber 
+   
+
+  $methods = $(Get-MsolUser -UserPrincipalName $_).StrongAuthenticationMethods 
+  $authmethods = $methods |Select-Object -Property MethodType
+   Write-Host $authmethods
+ 
+   $_ +","+$valuesUser+","+$authmethods >> 000-BTSS_output.csv
+   if($valuesUser)
+   {
+
+    $_ +","+$email+","+"Yes"+$authmethods >> 000-BTSS_EnrollmentStatus.csv
+    }
+    else
+    {
+     $_ +","+$email+","+"No" +$authmethods >> 000-BTSS_EnrollmentStatus.csv
+    }
+
+  # Write-Host  $_ +","+$valuesUser+","+$authmethods
+ }
+ #write-host "MFA details of user" $Script:args "are " $userdetail
+
